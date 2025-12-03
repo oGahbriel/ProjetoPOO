1968cd6966108ac21378c3664eb4164993176467
Gabriel Gomes <gabrielgomes7703@gmail.com>
2025-12-03 18:39:41 +0000

    v2-Projeto-Dia2

--- Sources/Projeto-Dia2/main.swift
+++ Sources/Projeto-Dia2/main.swift
@@ -1,26 +1,3 @@
-/*
-Crie a classe AulaColetiva, que também herda de Aula:
-○ Esta é outra especialização, representando uma aula em grupo com regras
-específicas.
-
-○ Propriedades Adicionais:
-■ alunosInscritos: Um dicionário ([String: Aluno]) para
-armazenar os alunos usando a matrícula como chave. A lista deve ser
-private(set).
-
-■ capacidadeMaxima (Int), com um valor padrão de 25.
-
-○ Método Adicional: Crie a função inscrever(aluno: Aluno) -> Bool.
-Ela deve conter a seguinte lógica:
-
-○ Sobrescrita de Método: Sobrescreva getDescricao() para incluir o
-número de vagas ocupadas e a capacidade máxima da turma.
-*/
-
-
-
-
-
 class AulaColetiva: Aula{
 
     private(set) var alunosInscritos:  [String: Aluno] =[:]
@@ -48,11 +25,15 @@ class AulaColetiva: Aula{
              print ("Erro: Capacidade Maxima Excedida")
              return false 
         }
+    }
 
+    override func getDescricao() -> String{
+        return """
+        Informações Pessoais:
+        Nome: \(self.nome)
+        Email: \(self.email)
+        Vagas: \(alunosInscritos.count) / \(capacidadeMaxima)
+
+        """
     }
 }
-
-■ Verificar se a aula já atingiu a capacidade máxima.
-■ Verificar se o aluno já não está inscrito.
-■ Adicionar o aluno ao dicionário alunosInscritos.
-■ Imprimir mensagens de sucesso, aviso ou erro para cada caso.
