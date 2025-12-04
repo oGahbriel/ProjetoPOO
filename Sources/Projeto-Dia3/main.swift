fb8407fd0c866d96396197af3ea6d5cfc44d01df
Gabriel Gomes <gabrielgomes7703@gmail.com>
2025-12-04 02:57:52 +0000

    v1-Projeto-Dia3

--- Sources/Projeto-Dia3/main.swift
+++ Sources/Projeto-Dia3/main.swift
@@ -1,3 +1,4 @@
+// 1 //
 class Academia {
     let nome: String 
     private var alunosMatriculados: [String: Aluno]
@@ -6,11 +7,48 @@ class Academia {
     private var aulasDisponiveis: [Aula]
 
     init(nome: String, alunosMatriculados: String, intrutoresContratados: String,
-         aparelhos: aparelho, aulasDisponiveis: Aula){
+         aparelhos: Aparelho, aulasDisponiveis: Aula){
                 self.nome = nome
                 self.alunosMatriculados = alunosMatriculados
                 self.intrutoresContratados = intrutoresContratados
                 self.aparelhos = aparelhos
                 self.aulasDisponiveis = aulasDisponiveis
+    }
+
+// 2 //
+
+    func buscarAluno(matricula: String) -> Aluno?{
+        if alunosMatriculados.keys.contains(aluno.matricula){
+            return alunosMatriculados[aluno.matricula]
+        }else {
+            return nil
+        }
+    }
+
+    func listarAlunos(){
+        print ("--- Lista de Alunos Matriculados ---")
+        let alunos: infoAlunos (alunosMatriculados.values)
+
+        if alunos.empty{
+            print("Nenhum aluno matriculado")
+        }else{
+            let alunosOrdenados = alunos.sorted
+
+            for aluno in alunosOrdenados {
+                print (aluno.getDescricao())
+            }
+        }
+        print("---------------------------------------------------")
+    }
+
+    func listarAulas(){
+        print ("--- Lista de Aulas ---")
+        let aulas: listaAulas (aulasDisponiveis.values)
+
+        for aula in listaAulas{
+            print (aula.getDescricao())
+        }
+
+        print("---------------------------------------------------")
     }
 }
